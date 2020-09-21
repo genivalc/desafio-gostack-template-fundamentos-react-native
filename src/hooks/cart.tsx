2067a94c1cddd12a4b8c141b66d4502ab80d4112
Genival Neto <genivalcandeiadon.neto@gmail.com>
2020-09-21 13:18:15 -0300

    Projeto concluido

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -8,6 +8,7 @@ import React, {
 
 import AsyncStorage from '@react-native-community/async-storage';
 
+
 interface Product {
   id: string;
   title: string;
@@ -23,6 +24,9 @@ interface CartContext {
   decrement(id: string): void;
 }
 
+const Storage = '@GoMarketplace:products';
+
+
 const CartContext = createContext<CartContext | null>(null);
 
 const CartProvider: React.FC = ({ children }) => {
@@ -30,23 +34,71 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const storageProducts = await AsyncStorage.getItem(
+        `${Storage}`,
+      ); // buscar todos os produtos
+
+      //verificar os produtos e passar
+      if (storageProducts) {
+        setProducts([...JSON.parse(storageProducts)]);
+      }
     }
 
     loadProducts();
   }, []);
 
   const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+    // Verificar se  o produto existe
+    const productExists = products.find(p => product.id === p.id);
+
+    //verificar se os produto ja foi adicionado e caso esteja da mais um
+    if (productExists) {
+      setProducts(
+        //mapeando todos os produtos e verificando se os mesmos são iguais
+        products.map(p =>
+          p.id === product.id ? { ...product, quantity: p.quantity + 1 } : p,
+        ),
+      );
+    } else {
+      //se não existir vai criar um e adicionar
+      setProducts([...products, { ...product, quantity: 1 }]);
+    }
+
+    await AsyncStorage.setItem(
+      `${Storage}`,
+      JSON.stringify(products),
+    );
+
+  }, [products]);
 
   const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+
+    //verificar se a id existe e adicionar
+    const newProducts = products.map
+      (product => product.id === id ?
+        { ...product, quantity: product.quantity + 1 } : product);
+
+    setProducts(newProducts);
+
+    await AsyncStorage.setItem(
+      `${Storage}`,
+      JSON.stringify(newProducts),
+    );
+  }, [products]);
 
   const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+    //verificar se a id existe e decremetar
+    const newProducts = products.map
+      (product => product.id === id ?
+        { ...product, quantity: product.quantity - 1 } : product);
+
+    setProducts(newProducts);
+
+    await AsyncStorage.setItem(
+      `${Storage}`,
+      JSON.stringify(newProducts),
+    );
+  }, [products]);
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -39,23 +39,36 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    //pega o valor
+    const total = products.reduce((accumulator, product) => {
+      //multiplica o os preço com a quantidade
+      const productsSubtotal = product.price * product.quantity;
+      //adicionar o acumaldor com o subtotal
+      return accumulator + productsSubtotal;
+      //passa o valor inicial como zero
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+      const total = products.reduce((accumulator, product) => {
+        //Retorna a quantidade
+        const productsQuantity = product.quantity;
+        //adicionar o acumaldor com a quantidade
+        return accumulator + productsQuantity;
+        //passa o valor inicial como zero
+      }, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
